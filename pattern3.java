596a0191147253a03bb05df7b6fb487acda34454
Vshwas0410 <vishwassingh0441097@gmail.com>
2024-01-31 14:11:17 +0530

    nn

--- pattern3.java
+++ pattern3.java
@@ -4,12 +4,12 @@ public class pattern3 {
             for(int j=1;j<=i;j++)
             
                 System.out.print("*");
-            
+            {
                     
                     System.out.println();
                 }
             }
         }
-    
+    }
     
 
